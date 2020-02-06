13994807e8183c8d99fba0b2bad6c9d38ddba287
Umenyan <wonder.paso27@gmail.com>
2020-02-06 16:02:54 +0900

    ユーザの入力に応じて複数の画像を返す機能の実装

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -28,7 +28,9 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           input_text = event.message['text'].downcase
-          client.reply_message(event['replyToken'], generate_line_massage_array(input_text))
+          responce = client.reply_message(event['replyToken'], generate_line_massage_array(input_text))
+          p responce.message
+          p JSON.parse(responce.body)
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
@@ -39,8 +41,8 @@ class WebhookController < ApplicationController
     head :ok
   end
 
-  def generate_message(text)
-    case text
+  def generate_message(input_text)
+    case input_text
     when "ねこ", "猫", "ネコ", "neko","cat"
       return "にゃんにゃん"
     when "いぬ", "犬", "イヌ", "inu", "dog"
@@ -50,18 +52,18 @@ class WebhookController < ApplicationController
     end
   end
 
-  def generate_line_text_hash(text)
+  def generate_line_text_hash(input_text)
     return {
       type: 'text',
-      text: generate_message(text)
+      text: generate_message(input_text)
     }
   end
 
-  def call_bing_image_search_api(text)
+  def call_bing_image_search_api(input_text)
     uri  = "https://api.cognitive.microsoft.com"
     path = "/bing/v7.0/images/search"
     
-    uri = URI("#{uri}#{path}?q=#{URI.escape(text)}")
+    uri = URI("#{uri}#{path}?q=#{URI.escape(input_text)}")
     
     request = Net::HTTP::Get.new(uri)
     request['Ocp-Apim-Subscription-Key'] = ENV["BING_IMAGE_API_KEY"]
@@ -76,33 +78,64 @@ class WebhookController < ApplicationController
   def generate_line_image_hash(json)
     random_result = json.sample
     
-    originalContentUrl = random_result["contentUrl"]
-    previewImageUrl = random_result["thumbnailUrl"]
+    original_content_url = random_result["contentUrl"]
+    preview_image_url = random_result["thumbnailUrl"]
 
     return {
       type: 'image',
-      originalContentUrl: replace_to_https(originalContentUrl),
-      previewImageUrl: replace_to_https(previewImageUrl) + "&c=4&w=240&h=240"
+      originalContentUrl: replace_to_https(original_content_url),
+      previewImageUrl: replace_to_https(preview_image_url) + "&c=4&w=240&h=240"
     }
   end
 
-  def generate_line_text_hash_when_image_not_found
+  def generate_line_text_hash_when_image_not_found(input_text)
     return {
         type: 'text',
-        text: "ごめんね #{0x100098.chr('UTF-8')} \n 見つからなかったみたい…"
+        text: "ごめんね #{0x100098.chr('UTF-8')} \n#{input_text}は見つからなかったみたい… \nちがうどうぶつの名前を入れてみて#{0x10005C.chr('UTF-8')}"
       }
   end
 
-  def generate_line_massage_array(text)
-    json_value = call_bing_image_search_api(text)["value"]
+  def generate_line_massage_array(input_text)
+    json_value = call_bing_image_search_api(input_text)["value"]
     if json_value.blank?
-      return generate_line_text_hash_when_image_not_found
+      return generate_line_text_hash_when_image_not_found(input_text)
     else
-      return [generate_line_text_hash(text), generate_line_image_hash(json_value)]
+      return generate_line_image_carousel_hash(input_text, json_value.sample(10))
     end
   end
 
   def replace_to_https(url)
     return url.sub(/http:/, "https:")
+  end
+
+  def generate_line_image_carousel_columns_hash(json, input_text)
+    return {
+      imageUrl: replace_to_https(json["thumbnailUrl"]) + "&c=4&w=240&h=240",
+      action: {
+        type: "uri",
+        label: generate_message(input_text),
+        uri: json["hostPageUrl"]
+      }
+    }
+  end
+
+  def generate_line_image_carousel_columns_array(json, input_text)
+    image_carousel_columns = Array.new
+    for value in json
+      image_carousel_columns.push(generate_line_image_carousel_columns_hash(value, input_text))
+    end
+    puts image_carousel_columns
+    return image_carousel_columns
+  end
+
+  def generate_line_image_carousel_hash(input_text, json)
+    return {
+      type: 'template',
+      altText: "#{input_text}の画像",
+      template: {
+        type: "image_carousel",
+        columns: generate_line_image_carousel_columns_array(json, input_text)
+      }
+    }
   end
 end
